d548dc2cf81129b576f33ce214354edf9ef13916
GhBogdan97 <bogdyg97@gmail.com>
2018-03-11 16:50:10 +0200

    Added: Seed(Roles, User: username: God, email: god@iss.com, password: Password123.), Address

---
+++ BloodPlus/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodPlus
+{
+    public static class Configuration
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return "Data Source=localhost\\SQLEXPRESS01;Initial Catalog=BloodPlus;Integrated Security=True;";
+            }
+        }
+    }
+}
