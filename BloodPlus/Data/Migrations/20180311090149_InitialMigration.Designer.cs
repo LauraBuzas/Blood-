b933e318d8dc5455774c908db48a86ba9e354aa5
Liana Elisabeta <lianaelisabeta.ln@gmail.com>
2018-03-11 20:19:44 +0200

    Adding Center and Employee Classes + UnitTestsProject

--- BloodPlus/Data/Migrations/20180311090149_InitialMigration.Designer.cs
+++ BloodPlus/Data/Migrations/20180311090149_InitialMigration.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BloodPlus.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180311090149_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BloodPlus/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodPlus.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            EmployeeId = 0;
+            EmployeeName = "";
+            AdressId = 0;
+            CenterId = 0;
+        }
+        public Employee(int id,String name,int adrid,int centerid)
+        {
+            EmployeeId = id;
+            EmployeeName = name;
+            AdressId = adrid;
+            CenterId = centerid;
+        }
+        public int EmployeeId { get; set; }
+        public String EmployeeName { get; set; }
+        public int AdressId { get; set; }
+        public int CenterId { get; set; }
+    }
+}
