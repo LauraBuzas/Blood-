2b4e34c7e68868478a7db7e0bf8d232d26029a8b
Liana Elisabeta <lianaelisabeta.ln@gmail.com>
2018-03-11 20:19:44 +0200

    Adding Center and Employee Classes + UnitTestsProject

---
+++ BloodPlus/Models/Center.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodPlus.Models
+{
+    public class Center
+    {
+        public Center()
+        {
+            CenterId = 0;
+            CenterName = "";
+            AdressId = 0;
+            AvailableQuantity = 0;
+        }
+        public Center(int id,String name,int adrid,double qty)
+        {
+            CenterId = id;
+            CenterName = name;
+            AdressId = adrid;
+            AvailableQuantity = qty;
+        }
+        public int CenterId { get; set; }
+        public String CenterName { get; set; }
+        public int AdressId { get; set; }
+        
+        public double AvailableQuantity { get; set; }
+
+    }
+}
