0a8a2c95221985477acc5d4bb47f05073170ed71
Alex C <antonio_cornea@yahoo.com>
2018-03-13 17:56:23 +0200

    Created Patient and Request classes no FK

---
+++ BloodPlus/Data/Migrations/20180313155532_CreatedPatientAndHospitalClassesNoFK.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BloodPlus.Data.Migrations
+{
+    public partial class CreatedPatientAndHospitalClassesNoFK : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
