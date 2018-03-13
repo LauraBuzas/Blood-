beba7b716d2580d958d3fb283331b9ffc57f3966
Laura87510 <laurascurtu1997@yahoo.com>
2018-03-13 16:27:39 +0200

    Create Hospital and Doctor Migration

--- BloodPlus/Data/Migrations/20180313141056_CreateHospitalAndDoctorMigration.Designer.cs
+++ BloodPlus/Data/Migrations/20180313141056_CreateHospitalAndDoctorMigration.Designer.cs
@@ -4,14 +4,17 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Storage.Internal;
 using System;
 
 namespace BloodPlus.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180313141056_CreateHospitalAndDoctorMigration")]
+    partial class CreateHospitalAndDoctorMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -94,30 +97,6 @@ namespace BloodPlus.Data.Migrations
                         .HasFilter("[NormalizedUserName] IS NOT NULL");
 
                     b.ToTable("AspNetUsers");
-                });
-
-            modelBuilder.Entity("BloodPlus.Models.Donor", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<string>("CNP")
-                        .IsRequired();
-
-                    b.Property<string>("Email")
-                        .IsRequired();
-
-                    b.Property<string>("FirstName")
-                        .IsRequired();
-
-                    b.Property<string>("LastName")
-                        .IsRequired();
-
-                    b.Property<string>("PhoneNumber");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("Donors");
                 });
 
             modelBuilder.Entity("BloodPlus.Models.Doctor", b =>
