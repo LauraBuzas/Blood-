d6163d0d6bca28e51b78b2a4976d2357c1de60e3
Liana Elisabeta <lianaelisabeta.ln@gmail.com>
2018-03-13 16:49:15 +0200

    removed fk props

--- UnitTestsProject/Models/CenterTest.cs
+++ UnitTestsProject/Models/CenterTest.cs
@@ -10,14 +10,13 @@ namespace UnitTestsProject
         {
             Center center = new Center();
             
-            center.AdressId = 125;
+            
             center.Id = 126;
             center.CenterName = "ReginaMaria";
             center.AvailableQuantity = 15.5;
 
             Assert.IsTrue(center.CenterName.Equals("ReginaMaria"));
             Assert.IsTrue(center.Id == 126);
-            Assert.IsTrue(center.AdressId == 125);
             Assert.IsTrue(center.AvailableQuantity == 15.5);
 
            
