c8367f8df3fd01a131ca7053a7425c7a7187b13d
Alex C <antonio_cornea@yahoo.com>
2018-03-13 16:16:16 +0200

    CreatedPatientAndRequestClasses

---
+++ BloodPlus/Models/Patient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BloodPlus.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string CNP { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        //CodA
+        public int IdAddress { get; set; }
+
+        //CodD
+        public int IdDoctor { get; set; }
+
+  
+
+    }
+}
