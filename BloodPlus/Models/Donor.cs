1a496416daba80052893e9ff42afb522bd3279e1
nicucomiati990 <nicu.comiati@gmail.com>
2018-03-13 14:53:50 +0200

    added Donor class with tests

---
+++ BloodPlus/Models/Donor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodPlus.Models
+{
+    public class Donor
+    {
+
+        public Donor(int id, string CNP, string firstName, string lastName, string email, string phone)
+        {
+            this.Id = id;
+            this.CNP = CNP;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Email = email;
+            this.PhoneNumber = phone;
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        public string CNP { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+
+
+    }
+}
