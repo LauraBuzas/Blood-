d6820897b68ba83dd315ca6e00e38dab2ee32032
GhBogdan97 <bogdyg97@gmail.com>
2018-06-03 17:46:16 +0300

    fix adding analyses

--- DatabaseAccess/Data/Migrations/20180603142922_IsAnalysisFilledMigration.Designer.cs
+++ DatabaseAccess/Data/Migrations/20180603142922_IsAnalysisFilledMigration.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace DatabaseAccess.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180603142922_IsAnalysisFilledMigration")]
+    partial class IsAnalysisFilledMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
